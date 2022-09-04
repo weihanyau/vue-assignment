f81452d32ad5e6ced0924d49dcc1b4cdb05c8bcb
Wei Han Yau <weihanyau@gmail.com>
2022-09-04 17:05:41 +0800

    Done home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,33 @@
+<script>
+export default {
+  data() {
+    return {
+      timer: null,
+      currentTime: new Date().toLocaleString(),
+    };
+  },
+  mounted() {
+    this.timer = setInterval(() => {
+      this.currentTime = new Date().toLocaleString();
+    }, 1000);
+  },
+  unmounted() {
+    clearInterval(this.timer);
+  },
+};
+</script>
+
 <template>
-  <div>In home view.</div>
+  <div class="container">
+    <p>Hello this is the onboarding assignment!</p>
+    <p>Current Time: {{ currentTime }}</p>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
